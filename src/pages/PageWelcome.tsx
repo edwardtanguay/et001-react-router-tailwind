1a350fd47b9399b4434dc3f003cedec9246d36ed
Edward Tanguay (windows) <edward@tanguay.info>
2023-05-14 14:25:36 +0200

    tailwind/sass in

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,7 +1,8 @@
 export const PageWelcome = () => {
 	return (
-		<>
+		<div className="page pageWelcome">
 			<p className="text-red-600">Welcome to this site.</p>
-		</>
+			<p>This is some info.</p>
+		</div>
 	);
 };
