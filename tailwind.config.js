5d8b7109c28dde222ad7323f3b29f8334f5e3fa1
Edward Tanguay (windows) <edward@tanguay.info>
2023-05-14 16:25:47 +0200

    took out fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,20 +9,6 @@ export default {
 
   theme: {
     extend: {
-      colors:{
-        // Dracula Theme
-        'BACKGROUND':'#282a36',
-        'FOREGROUND':'#f8f8f2',
-        'CURRENTLINE':'#44475a',
-        'COMMENT':'#6272a4',
-        'CYAN':'#8be9fd',
-        'GREEN':'#50fa7b',
-        'ORANGE':'#ffb86c',
-        'PINK':'#ff79c6',
-        'PURPLE':'#bd93f9',
-        'RED':'#ff5555',
-        'YELLOW':'#f1fa8c',
-      },
       fontFamily:{
         'Poppins':'Poppins',
         'Shadows': 'Shadows Into Light'
